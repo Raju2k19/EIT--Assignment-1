9fe00f30d8d28b36dbf3a2589d15212e31a8b239
Rejaul Islam <rejaraju2k19@gmail.com>
2022-11-04 05:27:09 +0600

    loop

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ document.write(number, "<br>");
 
 
 
-// <-8. Using the every metho  : ->
+// <-8. Using the every,filter,find,map metho  : ->
 var marks = [ 40, 45, 37, 20];
 let string = ['raju', 'modhu', 'morsalin', 'hanif']
 var result = marks.every(function (num) {
@@ -80,3 +80,102 @@ document.write(result3, "<br>")
 document.write(result1, "<br>");
 document.write(result4, "<br>")
 document.write(result5, "<br>");
+
+// <-14. Using the sort metho  : ->
+let alfa = ["A","B", "C","D"];
+alfa.sort();
+document.write(alfa ,"<br>");
+
+
+// <-15. Using the fill metho  : ->
+var device = ["samsung", "iphone", "ipad", "oppo", "vivo"]
+device.fill("Nokiya",2,3);
+document.write(device,"<br>");
+
+
+// <-16. Using the from metho  : ->
+document.write(Array.from('football'));
+document.write("<br>")
+
+
+// <-17. Using the from method  : ->
+
+let digit = [1,2,3,4,5];
+let rst = Array.isArray(digit);
+document.write(rst, "<br>");
+
+// <-18. Using the forEach method  : ->
+var names2 = ["shuvo", "rony", "jahid", "rubel"];
+names2.forEach(name => document.write(name,'<br>'));
+
+
+
+
+// <- Write a program for concatenation two array : ->
+// --------------------PROGRAM--------------------
+
+let song1 = ["Amar","Sonar","Bangla"];
+let song2 =  ["Ami", "Tomay", "Valobashi"];
+
+let rst2 = ([...song1,...song2]);
+document.write(rst2,"</br>");
+
+
+//  <-  Task related to Object ->:
+// <- Write a program for entries()->:
+const team = ["bangladesh","india","Pakstian", "South-Africa"];
+const match = team.entries()
+for (let entry of match) {
+    document.write(entry, "</br>");
+}
+
+// <- Write a program for hasOwn ()->:
+
+let bestOne = {  
+    name: 'Tesla',  
+    market: 'hit',  
+}  
+  
+let song = {  
+    name: 'amar sonar bangla',  
+}  
+
+let object = bestOne.hasOwnProperty('name'); 
+document.write(object,"</br>");
+
+let object2 = bestOne.hasOwnProperty('type'); 
+document.write(object2,"</br>");
+
+
+
+
+// <- Write a program for loop -> :
+
+document.write("Write a program for loop")
+for(let i = 1; i<=6; i++){
+    document.write("</br>",i);
+}
+
+
+// <- Write a program for of loop -> :
+document.write('<br>','for of loop:')
+const num = [1,2,3,4,5,6,7,8];
+
+for(const number of num){
+    document.write('<br>',number);
+}
+
+
+
+// <- Write a program for in loop -> :
+const deatils = {
+    name:'Mohammad',
+    nickName: 'Rejaul',
+    relegion: "Islam"
+}
+for(const x in deatils){
+    // console.log(man)
+    document.write('<br>',x)
+}
+
+
